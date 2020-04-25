b7edb996f6893c53f9186378812256aa5f72dd0a
Fabricio Sena <fabricior.sena@outlook.com>
2020-04-25 17:35:51 -0300

    adiciona estilo na pagina de logon

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { LogIn } from 'react-feather';
+
+import './styles.css';
+
+import heroesImg from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
+
 
 import './styles.css';
 
 export default function Logon()
 {
     return (
-        <h1>Hello World login</h1>
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero"/>
+                <form>
+                    <h1>Faça seu logon</h1>
+
+                    <input placeholder="Sua ID"/>
+                    <button className="button" type="submit">Entrar</button>
+                    
+                    <a href="/register">
+                        <LogIn size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </a>
+                </form>
+            </section>
+            <img src={heroesImg} alt="Heroes "/>
+        </div>
     );
 }
