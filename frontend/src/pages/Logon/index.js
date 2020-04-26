7b5831e89410f1b04fbdf86727ab6b748616919c
Fabricio Sena <fabricior.sena@outlook.com>
2020-04-26 01:45:45 -0300

    adiciona sistema de login

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { LogIn } from 'react-feather';
 
+import api from '../../services/api';
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
@@ -12,14 +13,42 @@ import './styles.css';
 
 export default function Logon()
 {
+    const [id, setId] = useState('');
+
+    const history = useHistory();
+
+    async function handlerLogin (e)
+    {
+        e.preventDefault();
+
+        try
+        {
+            const response = await api.post('/sessions', { id });
+            
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/profile');
+        }
+        catch (err)
+        {
+            alert('Erro no login, tente novamente');
+        }
+
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Hero"/>
-                <form>
+                <form onSubmit={handlerLogin}>
                     <h1>Faça seu logon</h1>
 
-                    <input placeholder="Sua ID"/>
+                    <input 
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={e=>setId(e.target.value)}
+                    />
                     <button className="button" type="submit">Entrar</button>
                     
                     <Link className="back-link" to="/register">
